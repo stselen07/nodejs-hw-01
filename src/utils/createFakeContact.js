0114a15433e77e2df1b42d4b0fb3eaad4fbad82a
sofia tselen <sofiatselenx@gmail.com>
2024-06-12 01:31:10 +0300

    final fix

--- src/utils/createFakeContact.js
+++ src/utils/createFakeContact.js
@@ -1,7 +1,6 @@
 import { faker } from "@faker-js/faker";
 
 export const createFakeContact = () => ({
-  id: faker.datatype.uuid(),
   name: faker.person.fullName(),
   phone: faker.phone.number(),
   email: faker.internet.email(),
