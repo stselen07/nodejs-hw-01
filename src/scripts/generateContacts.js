9a8ec6b8e94390f5a54776799432882d7ea734ef
sofia tselen <sofiatselenx@gmail.com>
2024-06-14 15:58:08 +0300

    fix generate

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,19 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
-import fs from "fs/promises";
-import { createFakeContact } from "../utils/createFakeContact.js";
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from 'fs/promises';
+
 const generateContacts = async (number) => {
-    const contacts = [];
-    for (let i = 0; i <= number; i++){
-        contacts.push(createFakeContact());
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    for (let i = 0; i < number; i++) {
+      contacts.push(createFakeContact());
     }
-    try {
-        const data = await fs.readFile(PATH_DB, "utf-8");
-        const storageContacts = JSON.parse(data);
-        const generateContacts = [...storageContacts, ...contacts];
-        await fs.writeFile(PATH_DB, JSON.stringify(generateContacts));
-        } catch (error) {
-        console.error("somthing went wrong", error);
-    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    //   console.log(contacts);
+    return contacts;
+  } catch (error) {
+    console.log('Error:', error.message);
+  }
 };
 
-await generateContacts(1);
+await generateContacts(5);
