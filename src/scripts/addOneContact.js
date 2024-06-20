5d0aa6916712d6c75f08f6a6dd92040b1e42caea
sofia tselen <sofiatselenx@gmail.com>
2024-06-21 01:03:01 +0300

    update add one

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,21 +1,17 @@
 import { PATH_DB } from '../constants/contacts.js';
-import fs from "fs/promises";
-import { createFakeContact } from "../utils/createFakeContact.js";
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
 export const addOneContact = async () => {
-    try {
-    
-    const contacts = []; 
-        contacts.push(createFakeContact());
-        const data = await fs.readFile(PATH_DB, "utf-8");
-        const storageContacts = JSON.parse(data);
-        const addedOneContact = [...storageContacts, ...contacts];
-        await fs.writeFile(PATH_DB, JSON.stringify(addedOneContact));
-        
-} catch (error) {
-        console.error("somthing went wrong", error);
-    }
-    
+  try {
+    const fileContent = await fs.readFile(PATH_DB, 'utf8');
+    const existingContacts = JSON.parse(fileContent);
+
+    existingContacts.push(createFakeContact());
+    await fs.writeFile(PATH_DB, JSON.stringify(existingContacts));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-await addOneContact();
+addOneContact();
